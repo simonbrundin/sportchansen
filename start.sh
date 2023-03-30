673bc12cb83723021b4cf8e6b163cca6c7d8c168
Simon Brundin <simonbrundin@gmail.com>
2023-03-30 08:50:05 +0200

    go to repo folder

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
-set PATH_TO_REPOS_FOLDER '~/repos'
-
+# Gå till mapp med repos
+set PATH_TO_REPOS_FOLDER 'Repos'
+cd
 cd $PATH_TO_REPOS_FOLDER
 
 # git clone https://github.com/simonbrundin/new-project.git
