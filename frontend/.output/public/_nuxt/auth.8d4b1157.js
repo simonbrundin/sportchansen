be67334805fbea1b9c2aac4015c6cfe9acc608a5
Simon Brundin <simonbrundin@gmail.com>
2023-04-18 16:26:37 +0200

    updates

--- frontend/.output/public/_nuxt/auth.8d4b1157.js
+++
@@ -1,1 +0,0 @@
-import{A as t}from"./entry.0447124f.js";const r=t({id:"auth-store",state:()=>({signInErrorMessage:""}),actions:{},getters:{}});export{r as u};
--- frontend/.output/public/_nuxt/user.ee5a74aa.js
+++
@@ -1,1 +0,0 @@
-import{z as e,h as c,o as r}from"./entry.0447124f.js";const o={};function t(n,s){return r(),c("div")}const _=e(o,[["render",t]]);export{_ as default};
