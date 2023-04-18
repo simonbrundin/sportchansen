fa0c5197d602f071e49eb94b7a2ada98ed42db88
simonbrundin <54498413+simonbrundin@users.noreply.github.com>
2023-04-18 14:37:12 +0000

    Deploying to gh-pages from @ simonbrundin/sportchansen@be67334805fbea1b9c2aac4015c6cfe9acc608a5 🚀

---
+++ _nuxt/default.0a86bbf2.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./_plugin-vue_export-helper.c27b6911.js";import{h as o,L as r,o as s}from"./entry.f8192def.js";const a={},c={id:"default-layout"};function n(e,_){return s(),o("div",c,[r(e.$slots,"default")])}const l=t(a,[["render",n]]);export{l as default};
