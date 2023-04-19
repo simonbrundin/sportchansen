a1b89e9e79a657ceb022e2c1bbb8b0af364cdb03
Simon Brundin <simonbrundin@gmail.com>
2023-04-19 08:37:20 +0200

    t

--- frontend/pages/booked.vue
+++ frontend/pages/booked.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-slate-700 m-4 p-8 h-full text-white">
-    <h1 class="text-2xl">Tiden är bokad :)</h1>
+    <h1 class="text-2xl">Tiden är bokad! Nu jävlar</h1>
     <div class="text-rose-400">
       <div class="i-fluent-emoji-flat-woman-mechanic-light"></div>
 
